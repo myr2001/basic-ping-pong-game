0855830eda8f78cc9d3feed25c1f9876a729a96d
Myr <88000512+myr2001@users.noreply.github.com>
2025-02-12 21:49:16 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -10,10 +10,10 @@ background_color = (130, 210, 255) # RGB
 window.fill(background_color)
 
 # ----- MÜZİK -----
-mixer.init() # initialize (başlat)
-mixer.music.load("retromusic.ogg")
-mixer.music.play() # arkaplan olarak ayarla
-mixer.music.set_volume(0.05) # müzik sesi kısma (0: ses yok, 1: en yüksek)
+# mixer.init() # initialize (başlat)
+# mixer.music.load("retromusic.ogg")
+# mixer.music.play() # arkaplan olarak ayarla
+# mixer.music.set_volume(0.05) # müzik sesi kısma (0: ses yok, 1: en yüksek)
 
 # ----- GAMESPRITE SINIFI -----
 class GameSprite(sprite.Sprite): # üst sınıf: Sprite, alt sınıf: GameSprite
@@ -46,8 +46,70 @@ class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
         # W tuşa basılıysa ve ekranın yukarısına dayanmadıysa:
-        if keys["K_w"] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         # S tuş basılıysa ve ekranın altına dayanmadıysa:
-        if keys["K_s"] and self.rect.y < 495:
+        if keys[K_s] and self.rect.y < 495:
             self.rect.y += self.speed
+
+# ----- SPRITE TANIMLAMA -----
+racket1 = Player("racket1.png", 4, 30, 200, 50, 150) # oyuncu 1
+racket2 = Player("racket2.png", 4, 30, 200, 50, 150) # oyuncu 2
+ball = GameSprite("ball.png", 4, 200, 200, 50, 50) # top
+
+# ----- YAZILAR -----
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render("PLAYER 1 LOST!", True, (180,0,0))
+lose2 = font.render("PLAYER 2 LOST!", True, (180,0,0))
+
+speed_x = 3
+speed_y = 3
+
+# ----- OYUN DÖNGÜSÜ -----
+game = True # döngü değişkeni (çarpıya basınca oyun kapansın)
+finish = False # döngü değişkeni (oyunu kazanınca oyun kapansın)
+clock = time.Clock() #zamanlayıcı yarat
+FPS = 60 # frames per second (saniye başına düşen kare)
+
+while game: # oyun döngüsü
+    # ----- OYUNDAN ÇIKMA -----
+    for e in event.get(): # oyundaki olayları tara
+        if e.type == QUIT: # oyundan çık olayı varsa:
+            game = False # döngüyü durdur
+    
+    # ----- OYUN BİTMEDİYSE: -----
+    if finish != True:
+        window.fill(background_color) # arkaplan rengini güncelle
+        racket1.update_l() # oyuncu 1'in hareketlerini güncelle
+        racket2.update_r() # oyuncu 2'nin hareketlerini güncelle
+        # topun hareketlerini güncelle:
+        ball.rect.x += speed_x # topun x pozisyonuna speed x kadar ekle (yatay)
+        ball.rect.y += speed_y # topun y pozisyonuna speed y kadar ekle (dikey)
+
+        # ----- ÇARPIŞMALAR -----
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        # oyuncu 1 ve top çarpışıyorsa yada oyuncu 2 ve top çarpışıyorsa:
+            speed_x *= -1 # yatay hızı ters çevir (sektir)
+        
+        if ball.rect.y > 450 or ball.rect.y < 0:
+        # eğer top pencerenin en aşağısına yada en yukarısına çarptıysa:
+            speed_y *= -1 # dikey hızı ters çevir (sektir)
+        
+        if ball.rect.x < 0:
+        # eğer top pencerenin sol tarafına dokunduysa:
+            finish = True # oyun bitsin
+            window.blit(lose1, (200,200)) # oyuncu 1 kaybetti yazısını pencerenin ortasına getir
+        
+        if ball.rect.x > 700:
+        # eğer top pencerenin sağ tarafına dokunduysa:
+            finish = True # oyun bitsin
+            window.blit(lose2, (200,200)) # oyuncu 2 kaybetti yazısını pencerenin ortasına getir
+    
+    # ekrandaki konumların güncellenmesi:
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+# ekranın güncellenmesi ve zamanlayıcının çalıştırılması:
+display.update()
+clock.tick(FPS)
